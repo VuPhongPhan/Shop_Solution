0b298c0afab11b532734df257ad25da958b1b08b
vu phong <vuphongshyn1998@gmail.com>
2020-09-21 10:49:33 +0700

    Add project files.

---
+++ Shop_Solution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shop_Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
